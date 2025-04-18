b4916f2d3312c4c2c6fdf812466b6078ee0503fb
Aswini-Ramesh123 <achuachu9247@gmail.com>
2025-04-18 20:59:58 +0530

    delete

--- contactbook.py
+++ contactbook.py
@@ -1,4 +1,7 @@
-for i in range(1,6):
-    print()
-    for j in range(1,i+1):
-        print(j,end=" ")
+thisdict={
+    "name":"ashu",
+    "age":22,
+    "degree":"MCA"
+}
+for x,y in thisdict.items():
+    print(x,y)
